e4e7e973d38198b907534d436f3846ab2d0c8854
fabriciodigennaro <slither33f@gmail.com>
2024-09-13 14:01:13 +0200

    Add integration test for JmsEmailService

--- src/main/java/com/parkingapp/notificationservice/infrastructure/config/EmailNotificationConfig.java
+++ src/main/java/com/parkingapp/notificationservice/infrastructure/config/EmailNotificationConfig.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class EmailNotificationConfig {
@@ -16,5 +19,27 @@ public class EmailNotificationConfig {
             @Value("${spring.email.sender-email}") String sender
     ) {
         return new JmsEmailService(javaMailSender, sender);
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender(
+            @Value("${spring.email.host}") String host,
+            @Value("${spring.email.port}") int port,
+            @Value("${spring.email.sender-email}") String user,
+            @Value("${spring.email.password}") String password,
+            @Value("${spring.email.properties.mail.smtp.auth}") String smtpAuth,
+            @Value("${spring.email.properties.mail.smtp.starttls.enable}") String enableStartTls
+    ) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(user);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", enableStartTls);
+        return mailSender;
     }
 }
